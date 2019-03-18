67f4fd80fdac5cd74bcfc22f27a067329dc94dff
Jarrod Medrano <jmedran@gmail.com>
2019-03-17 18:04:11 -0600

    adding a sub form group

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,8 +12,10 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.signupForm = new FormGroup({
+      'userData': new FormGroup({
         'username': new FormControl(null, Validators.required),
-      'email': new FormControl(null, [Validators.required, Validators.email]),
+        'email': new FormControl(null, [Validators.required, Validators.email])
+      }),
       'gender': new FormControl('male')
     });
   }
